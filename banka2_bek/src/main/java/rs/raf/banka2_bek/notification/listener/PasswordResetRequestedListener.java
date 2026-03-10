b696b014d09892e0dc89b0c0a1ee61623d668768
sundjerbob <127345838+sundjerbob@users.noreply.github.com>
2026-03-10 23:47:49 +0100

    password change request notification aync impl

--- banka2_bek/src/main/java/rs/raf/banka2_bek/notification/listener/PasswordResetRequestedListener.java
+++ banka2_bek/src/main/java/rs/raf/banka2_bek/notification/listener/PasswordResetRequestedListener.java
@@ -1,6 +1,7 @@
 package rs.raf.banka2_bek.notification.listener;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import rs.raf.banka2_bek.auth.model.PasswordResetRequestedEvent;
 import rs.raf.banka2_bek.notification.service.MailNotificationService;
@@ -14,6 +15,7 @@ public class PasswordResetRequestedListener {
         this.mailNotificationService = mailNotificationService;
     }
 
+    @Async
     @EventListener
     public void onPasswordResetRequested(PasswordResetRequestedEvent event) {
         mailNotificationService.sendPasswordResetMail(event.getEmail(), event.getToken());
